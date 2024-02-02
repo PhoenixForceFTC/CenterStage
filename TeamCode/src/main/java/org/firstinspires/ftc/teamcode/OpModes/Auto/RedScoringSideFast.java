3f986f83fd06288fc5d7be0243a795c6805c4d04
rishibellam <119266305+rishibellam@users.noreply.github.com>
2024-02-01 18:53:56 -0600

    sepperated auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/RedScoringSideFast.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/RedScoringSideFast.java
@@ -3,22 +3,21 @@ package org.firstinspires.ftc.teamcode.OpModes.Auto;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.OpModes.Auto.RedScoringSideSubsystems.left;
+import org.firstinspires.ftc.teamcode.OpModes.Auto.RedScoringSideSubsystems.middle;
+import org.firstinspires.ftc.teamcode.OpModes.Auto.RedScoringSideSubsystems.right;
 import org.firstinspires.ftc.teamcode.Subsystems.Vision;
 
 @Config
 @Autonomous(group="!CompOpModes")
 public class RedScoringSideFast extends AutoOpMode {
     private Vision vision;
+    private left left;
+    private middle middle;
+    private right right;
     private Vision.IDENTIFIED_SPIKE_MARK_LOCATION spikeLocation;
     public static Position START = new Position(12, -63.375, 90);
     public static Position START_CENTER = new Position(12,-60,90);
-    public static Position SPIKE_LEFT = new Position(12, -36, 90);
-
-    public static Position SPIKE_LEFT_TURN = new Position(11, -36, 165);
-    public static Position SPIKE_LEFT_BACKUP = new Position(12, -44, 165);
-    public static Position SPIKE_LEFT_ADJUST_HEADING = new Position(12, -44, 90);
-    public static Position SPIKE_CENTER = new Position(12, -36, 90);
-    public static Position SPIKE_CENTER_BACKUP = new Position(12, -44, 90);
 
 
     public static Position SPIKE_RIGHT = new Position(25, -44.625, 90);
@@ -39,8 +38,6 @@ public class RedScoringSideFast extends AutoOpMode {
     public static Position DROP_POSITION_L = new Position(44, -28, 180);
     public static Position DROP_POSITION_TOUCH_BOARD_L = new Position(52, -28, 180);
     public static Position DROP_POSITION_BACKUP_BOARD_L = new Position(44, -28, 180);
-        public static Position RED_PILE_1_PREPOSITION = new Position(12, -12, 180);
-    public static Position RED_PILE_1_POSITION = new Position(-18, -16, 185);
     public static Position DROP_POSITION_R = new Position(44, -44, 180);
     public static Position DROP_POSITION_TOUCH_BOARD_R = new Position(52, -44, 180);
     public static Position DROP_POSITION_BACKUP_BOARD_R = new Position(44, -44, 180);
@@ -49,6 +46,9 @@ public class RedScoringSideFast extends AutoOpMode {
 
     @Override
     public void runOpMode() {
+        left = new left(this);
+        middle = new middle(this);
+        right = new right(this);
         vision = new Vision(this, Vision.START_POSITION.BLUE_LEFT);
         vision.initTfod();
 
@@ -68,60 +68,14 @@ public class RedScoringSideFast extends AutoOpMode {
         setStartPosition(START);
         switch (spikeLocation) {   // camer detection value goes herre
             case LEFT:
-
-                goTo(SPIKE_LEFT_TURN);
-                intake.returnPixel();
-                sleep(1000);
-                drop.goToPosition(3);
-                goTo(DROP_POSITION_TOUCH_BOARD_L);
-                topGate.setGateOpen();
-                sleep(1000);
-                topGate.setGateStopped();
-                splineTo(RED_PILE_1_PREPOSITION);
-                intake.stop();
-                drop.goToPosition(0);
-                goTo(RED_PILE_1_POSITION);
-
-
-                snagger.goToPosition(3);
-                telemetry.update();
-                int counter = 0;
-                intake.eatPixel();
-                while (counter < 40 && intake.getIntakePixels() < 2) {
-
-                    sleep(100);
-                    counter++;
-                    intake.pixelCounterTelemetry();
-                    telemetry.update();
-                }
-                intake.frontWheelReverse();
-                snagger.goToPosition(0);
-
-                sleep(1000);
-
-                intake.transferPixel();
-                sleep(2000);
-
-                
-
-                //goTo(SPIKE_LEFT_BACKUP);
-                //goTo(SPIKE_LEFT_ADJUST_HEADING);
-                //coneDrop(1);
-
+                left.go();
                 break;
             case MIDDLE:
-                goTo(SPIKE_CENTER);
-                intake.returnPixel();
-                sleep(2000);
-                goTo(SPIKE_CENTER_BACKUP);
-                coneDrop(2);
+                middle.go();
                 break;
             case RIGHT:
-                goTo(SPIKE_RIGHT);
-                intake.returnPixel();
-                sleep(2000);
-                goTo(SPIKE_RIGHT_BACKUP);
-                coneDrop(3);
+                right.go();
+                break;
         }
     }
     public void coneDrop(int casee) {
